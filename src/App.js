1d94e102ea81b51c25a4407dac12c16e97376066
Jon Oropeza <jon_oropeza@homedepot.com>
2017-05-29 10:48:39 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="app-header">
+          Header
+        </div>
+        <div className="app-hero">
+          Hero
+        </div>
+        <div className="app-content">
+          <p>Yo this is some awesome content.</p>
+          <p>Yo this is some awesome content.</p>
+          <p>Yo this is some awesome content.</p>
+          <p>Yo this is some awesome content.</p>
+          <p>Yo this is some awesome content.</p>
+          <p>Yo this is some awesome content.</p>
+          <p>Yo this is some awesome content.</p>
+          <p>Yo this is some awesome content.</p>
+          <p>Yo this is some awesome content.</p>
+          <p>Yo this is some awesome content.</p>
+          <p>Yo this is some awesome content.</p>
+        </div>
+        <div className="app-footer">
+          Footer
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;

6beb673ebdf40bebd94a6b77490da3b5995d15ef
Jon Oropeza <jon_oropeza@homedepot.com>
2017-05-31 07:53:47 -0700

    initial stopping point for header

--- src/App.js
+++ src/App.js
@@ -1,35 +1,68 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="app-header">
-          Header
+const App = () => (
+  <div className="App">
+    <div className="app-header">
+      <div className="nav">
+        <a className="nav-home">
+          <img
+            className="nav-logo"
+            alt="React Logo"
+            src="https://facebook.github.io/react/img/logo.svg"
+            height="36px"
+            width="36px"
+          />
+          React
+        </a>
+        <div className="nav-list-left">
+          <ul>
+            <li className="nav-list-item">
+              Docs
+            </li>
+            <li className="nav-list-item">
+              Tutorial
+            </li>
+            <li className="nav-list-item">
+              Community
+            </li>
+            <li className="nav-list-item">
+              Blog
+            </li>
+          </ul>
         </div>
-        <div className="app-hero">
-          Hero
-        </div>
-        <div className="app-content">
-          <p>Yo this is some awesome content.</p>
-          <p>Yo this is some awesome content.</p>
-          <p>Yo this is some awesome content.</p>
-          <p>Yo this is some awesome content.</p>
-          <p>Yo this is some awesome content.</p>
-          <p>Yo this is some awesome content.</p>
-          <p>Yo this is some awesome content.</p>
-          <p>Yo this is some awesome content.</p>
-          <p>Yo this is some awesome content.</p>
-          <p>Yo this is some awesome content.</p>
-          <p>Yo this is some awesome content.</p>
-        </div>
-        <div className="app-footer">
-          Footer
+        <div className="nav-list-right">
+          <ul>
+            <li className="nav-list-item">
+              GitHub
+            </li>
+            <li className="nav-list-item">
+              v15.5.4
+            </li>
+          </ul>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+    <div className="app-hero">
+      Hero
+    </div>
+    <div className="app-content">
+      <p>Yo this is some awesome content.</p>
+      <p>Yo this is some awesome content.</p>
+      <p>Yo this is some awesome content.</p>
+      <p>Yo this is some awesome content.</p>
+      <p>Yo this is some awesome content.</p>
+      <p>Yo this is some awesome content.</p>
+      <p>Yo this is some awesome content.</p>
+      <p>Yo this is some awesome content.</p>
+      <p>Yo this is some awesome content.</p>
+      <p>Yo this is some awesome content.</p>
+      <p>Yo this is some awesome content.</p>
+    </div>
+    <div className="app-footer">
+      Footer
+    </div>
+  </div>
+);
 
 export default App;

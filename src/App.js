61d33abcf2e112a099fd858fae4141728cfadc62
Jon Oropeza <jon_oropeza@homedepot.com>
2017-05-31 12:46:12 -0700

    candidate initial starting point

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
 import React from 'react';
 import './App.css';
-import Header from './components/header';
-
 
 const App = () => (
   <div className="App">
-    <Header />
-    <div className="app-hero">
-      Hero
-    </div>
-    <div className="app-content">
-      <p>Yo this is some awesome content.</p>
-      <p>Yo this is some awesome content.</p>
-      <p>Yo this is some awesome content.</p>
-      <p>Yo this is some awesome content.</p>
-      <p>Yo this is some awesome content.</p>
-      <p>Yo this is some awesome content.</p>
-      <p>Yo this is some awesome content.</p>
-      <p>Yo this is some awesome content.</p>
-      <p>Yo this is some awesome content.</p>
-      <p>Yo this is some awesome content.</p>
-      <p>Yo this is some awesome content.</p>
-    </div>
-    <div className="app-footer">
-      Footer
-    </div>
+    Hello World
   </div>
 );
 

b6ccfca994b08f3a98f43d57d3e288ed14eec0f1
Jon Oropeza <jon_oropeza@homedepot.com>
2017-06-01 15:42:07 -0700

    remove initial seed files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import './App.css';
-import Header from './components/header';
-import Hero from './components/hero';
-import Content from './components/content';
-import Footer from './components/footer';
 
 const App = () => (
   <div className="App">
-    <Header />
-    <Hero />
-    <Content />
-    <Footer />
+    Hello World
   </div>
 );
 

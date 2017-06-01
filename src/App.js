18d5483325b007d70bc2dfda02d0102715eb1da7
Jon Oropeza <jon_oropeza@homedepot.com>
2017-06-01 14:01:47 -0700

    interim commit with linting error

--- src/App.js
+++ src/App.js
@@ -3,8 +3,29 @@ import './App.css';
 
 const App = () => (
   <div className="App">
-    <div className="nav">
-      Header
+    <div className="header">
+      <div className="nav">
+        <div className="nav-left">
+          <div className="nav-logo">
+            <img src="https://facebook.github.io/react/img/logo.svg" />
+            React
+          </div>
+          <div className="nav-list">
+            <ul className="nav-list-items">
+              <li className="nav-list-item">Docs</li>
+              <li className="nav-list-item">Tutorial</li>
+              <li className="nav-list-item">Community</li>
+              <li className="nav-list-item">Blog</li>
+            </ul>
+          </div>
+          <div className="nav-form">
+            <input id="search" className="nav-search" />
+          </div>
+        </div>
+        <div className="nav-right">
+          Right Nav
+        </div>
+      </div>
     </div>
     <div className="hero">
       Hero

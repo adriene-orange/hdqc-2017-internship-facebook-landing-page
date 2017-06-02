d7c15664a26adf20e42f388bda729f44c59f2d39
Dylan Barnard <dylan_barnard@homedepot.com>
2017-06-02 09:30:54 -0700

    Inital footer commmit

---
+++ src/components/footer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './footer.scss';
+
+const Footer = function() {
+  <div className="footer-languages">
+    <ul></ul>
+    <li>English (US)</li>
+    <li>Español</li>
+    <li>Français (France)</li>
+    <li>中文(简体)</li>
+    <li>العربية</li>
+    <li>Português (Brasil)</li>
+    <li>Italiano</li>
+    <li>Deutsch</li>
+    <li>한국어</li>
+    <li>हिन्दी</li>
+    <li>日本語</li>
+  </div>
+
+
+};
+
+export default Footer;

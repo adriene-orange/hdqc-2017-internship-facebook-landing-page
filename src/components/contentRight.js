196f6115564f1281f483e6964aa6f73277002c99
Adriene Cranny <adrienecranny@HDC-WRK-M62.local>
2017-06-02 09:42:20 -0700

    Initial content right branch.

--- src/components/contentRight.js
+++ src/components/contentRight.js
@@ -2,4 +2,17 @@ import React from 'react';
 import './contentRight.css';
 
 
+const RightContent = () => (
+
+  <div className="RightContent">
+    <div className="RightHeader">
+      Sign Up
+      It&apos;s free and always will be.
+    </div>
+
+  </div>
+
+
+);
+
 export default RightContent;

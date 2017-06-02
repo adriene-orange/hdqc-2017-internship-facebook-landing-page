fa9144eb94b01bd385b8f1a24575a3032d010349
Jon Oropeza <jonathan_oropeza@homedepot.com>
2017-06-02 09:13:09 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './App.css';
+import Header from './components/header';
+import ContentLeft from './components/contentRight';
+import ContentRight from './components/contentRight';
+import Footer from './components/footer';
 
 const App = () => (
   <div className="App">
-    Hello World
+    <Header />
+    <div className="content">
+      <ContentLeft />
+      <ContentRight />
+    </div>
+    <Footer />
   </div>
 );
 

166fd8b556c1ca5a8daca5ee11517e22ed1a7a65
Von Doren Proctor <vondorenproctor@HDC-WRK-M61.local>
2017-06-02 14:10:53 -0700

    All header components exist. Spacing is mediocre.

--- src/components/header.js
+++ src/components/header.js
@@ -3,10 +3,34 @@ import './header.css';
 
 const Header = () => (
   <div className="header">
-    <div className="logo">
-      <img alt="Facebook Logo" height="36" width="36" src="./logo.svg" />
 
+    <div className="logo">
+      <img align="left" alt="Facebook Logo" height="100" width="100" src="https://www.seeklogo.net/wp-content/uploads/2011/08/facebook-logo-vector-400x400.png" />
     </div>
+
+    <form className="form-text">
+
+      Email or Phone <br />
+      <input type="text" name="" />
+
+    </form>
+    <form className="form-text">
+
+      Password <br />
+      <input type="text" name="" />
+
+      <br />
+      <a href="https://www.facebook.com/recover/initiate?lwv=110">
+          Forgot account?
+      </a>
+
+    </form>
+
+    <div className="button-container">
+      <input className="button" type="button" value="Log In" />
+    </div>
+
+
   </div>
 );
 

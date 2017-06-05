8372ed176c262dcc62a3a6e79a094283dd6f7349
Jon Oropeza <jon_oropeza@homedepot.com>
2017-06-05 10:34:23 -0700

    add failure conditions and tests to services

--- src/services/users.test.js
+++ src/services/users.test.js
@@ -10,34 +10,62 @@ const doesUserExist = user =>
   );
 
 describe('addUser', () => {
-  it('adds a new user', () => {
-    const newUser = { username: 'yo', password: 'filet' };
-    expect(doesUserExist(newUser)).to.equal(undefined);
-    serviceToTest.addUser(newUser.username, newUser.password)
-    .then(
-      () => {
-        expect(doesUserExist(newUser)).to.deep.equal(newUser);
-      },
-    );
+  describe('successes', () => {
+    it('adds a new user when passed in username and password parameters', () => {
+      const newUser = { username: 'yo', password: 'filet' };
+      expect(doesUserExist(newUser)).to.equal(undefined);
+      serviceToTest.addUser(newUser.username, newUser.password)
+      .then(
+        () => {
+          expect(doesUserExist(newUser)).to.deep.equal(newUser);
+        },
+      );
+    });
+  });
+  describe('failures', () => {
+    it('fails if the username is empty', (done) => {
+      const noUsername = { username: '', password: 'filet' };
+      serviceToTest.addUser(noUsername.username, noUsername.password)
+      .catch(
+        (error) => {
+          expect(error).to.deep.equal('addUser requires a valid username');
+          done();
+        },
+      );
+    });
+    it('fails if the password is empty', (done) => {
+      const noPassword = { username: 'itsMe', password: '' };
+      serviceToTest.addUser(noPassword.username, noPassword.password)
+      .catch(
+        (error) => {
+          expect(error).to.deep.equal('addUser requires a valid password');
+          done();
+        },
+      );
+    });
   });
 });
 
 describe('getUserByUsernameAndPassword', () => {
-  it('fetches a single existing user by username and password', () => {
-    const existingUser = { username: 'hdqc', password: 'hdqc' };
-    expect(doesUserExist(existingUser)).to.deep.equal(existingUser);
-    serviceToTest.getUserByUsernameAndPassword(existingUser.username, existingUser.password)
-    .then(
-      (user) => {
-        expect(doesUserExist(user)).to.deep.equal(existingUser);
-      },
-    );
+  describe('successes', () => {
+    it('fetches a single existing user by username and password', () => {
+      const existingUser = { username: 'hdqc', password: 'hdqc' };
+      expect(doesUserExist(existingUser)).to.deep.equal(existingUser);
+      serviceToTest.getUserByUsernameAndPassword(existingUser.username, existingUser.password)
+      .then(
+        (user) => {
+          expect(doesUserExist(user)).to.deep.equal(existingUser);
+        },
+      );
+    });
   });
-  it('rejects if a user with the username and password does not exist', () => {
-    const existingUser = { username: 'does not exist', password: 'nope' };
-    expect(doesUserExist(existingUser)).to.deep.equal(undefined);
-    serviceToTest.getUserByUsernameAndPassword(existingUser.username, existingUser.password)
-    .then(user => expect(user).to.not.be.ok)
-    .catch(error => expect(error).to.deep.equal('User not found'));
+  describe('failures', () => {
+    it('rejects if a user with the username and password does not exist', () => {
+      const existingUser = { username: 'does not exist', password: 'nope' };
+      expect(doesUserExist(existingUser)).to.deep.equal(undefined);
+      serviceToTest.getUserByUsernameAndPassword(existingUser.username, existingUser.password)
+      .then(user => expect(user).to.not.be.ok)
+      .catch(error => expect(error).to.deep.equal('User not found'));
+    });
   });
 });

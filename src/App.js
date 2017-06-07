17521e7c557a81a8efc5dce74987475ef9daac57
Adriene Cranny <adrienecranny@HDC-WRK-M62.local>
2017-06-07 11:51:13 -0700

    Removed changes to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './App.css';
-// import Header from './components/header';
-// import ContentLeft from './components/contentLeft';
-// import RightContent from './components/contentRight';
-// import Footer from './components/footer';
-import Home from './components/home';
+import Header from './components/header';
+import ContentLeft from './components/contentLeft';
+import RightContent from './components/contentRight';
+import Footer from './components/footer';
 
 const App = () => (
   <div className="App">
-    <Home />
+    <Header />
+    <div className="content-container">
+      <div className="content">
+        <ContentLeft />
+        <RightContent />
+      </div>
+    </div>
+    <Footer />
   </div>
 );
 

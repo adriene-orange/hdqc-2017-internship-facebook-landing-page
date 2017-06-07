7cf4f721d2e8ab051a85ae6aa27ab5ecb79bfe5a
Dylan Barnard <dylan_barnard@homedepot.com>
2017-06-07 11:08:41 -0700

    add jsdom-global package and modify mocha script

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
+// Libs
 import React from 'react';
 import server from 'react-dom/server';
 import cheerio from 'cheerio';
 import { expect } from 'chai';
+import { mount } from 'enzyme';
 
+// Components
 import App from './App';
 
 describe('App component', () => {
@@ -10,5 +13,10 @@ describe('App component', () => {
     const staticMarkup = server.renderToStaticMarkup(<App />);
     const $ = cheerio.load(staticMarkup);
     expect($.root().children().first()).to.be.ok;
+  });
+
+  it('renders success if true', () => {
+    const wrapper = mount(<App />);
+    expect(wrapper.find(Landing).length).to.be.equal(1);
   });
 });

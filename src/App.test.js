9a37fa40a65bfb01f0d136cd900831afc154ae73
Dylan Barnard <dylan_barnard@homedepot.com>
2017-06-07 11:25:33 -0700

    Add mounted component tests for App

--- src/App.test.js
+++ src/App.test.js
@@ -7,16 +7,23 @@ import { mount } from 'enzyme';
 
 // Components
 import App from './App';
+import Landing from './components/landing';
 
 describe('App component', () => {
+  const wrapper = mount(<App />);
+
   it('renders without crashing', () => {
     const staticMarkup = server.renderToStaticMarkup(<App />);
     const $ = cheerio.load(staticMarkup);
     expect($.root().children().first()).to.be.ok;
   });
 
-  it('renders success if true', () => {
-    const wrapper = mount(<App />);
+  it('renders Landing by default', () => {
     expect(wrapper.find(Landing).length).to.be.equal(1);
+  });
+
+  it('renders success message when state is set to true', () => {
+    wrapper.setState({ authenticated: true });
+    expect(wrapper.find('h1')).to.have.length(1);
   });
 });

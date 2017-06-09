a28343a98a6266b5bb522481a75320566decc10c
Keir Staple <keir_staple@homedepot.com>
2017-06-09 11:31:49 -0700

    Passing tests in Detail Wrapper.

--- src/components/detailWrapper.test.js
+++ src/components/detailWrapper.test.js
@@ -11,13 +11,15 @@ describe('Detail Wrapper', () => {
   const wrapper = shallow(<DetailWrapper />);
   it('renders without crashing', () => {
     const staticMarkup = server.renderToStaticMarkup(<DetailWrapper
-      value="test"
-      store={{ interests: [] }}
+      result={{}}
+      test=""
     />);
     const $ = cheerio.load(staticMarkup);
     expect($.root().first()).to.be.ok;
   });
   it('Detail component is found', () => {
+    wrapper.setState(
+      { result: { pages: { randomNumber: { thumbnail: { source: 'alskdfj' } } } } });
     expect(wrapper.find('Detail').length).to.be.equal(1);
   });
 });

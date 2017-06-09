33accbae7d7d83e4f77cff07c0f0a006980a6e48
Keir Staple <keir_staple@homedepot.com>
2017-06-09 11:45:34 -0700

    clean up api service

--- src/services/apiCall.js
+++ src/services/apiCall.js
@@ -4,23 +4,8 @@ require('isomorphic-fetch');
 const wikiUrl = 'https://en.wikipedia.org/w/api.php';
 const options = { headers: { 'Access-Control-Allow-Origin': '*' } };
 options.origin = 'https://en.wikipedia.org';
-// const options = { mode: 'no-cors' };
-
-// export const imageCall = (file) => {
-//   const apiUrl = `${commonsUrl}?action=query&origin=*&titles=${file}&prop
-// =imageinfo&iiprop=url&format=json`;
-//   return fetch(apiUrl)
-//     .then((response) => {
-//       if (response.status >= 400) {
-//         throw new Error('Bad response from server');
-//       }
-//       return response.json();
-//     })
-//     .then(data => data.query);
-// };
 
 export const apiCall = (title) => {
-  console.log('api call title is ', title);
   const apiUrl = `${wikiUrl}?action=query&origin=*&titles=${title}&prop=pageimages&imlimit=1&format=json&pithumbsize=500`;
   return fetch(apiUrl)
     .then((response) => {
@@ -30,6 +15,4 @@ export const apiCall = (title) => {
       return response.json();
     })
     .then(data => data.query);
-      // .pages[Object.keys(data.query.pages)[0]]
-      //   .images[0].title).replace(/ /g, '_'));
 };

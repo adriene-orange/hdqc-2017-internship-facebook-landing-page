b00f3d05df2f0e098dc867aba044a20b1f2f7f3e
Keir Staple <keir_staple@homedepot.com>
2017-06-12 13:06:53 -0700

    add redux logger

--- src/store.js
+++ src/store.js
@@ -1,4 +1,9 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
 import rootReducer from './reducers';
 
-export default initialState => createStore(rootReducer, initialState);
+export default initialState => createStore(
+  rootReducer,
+  initialState,
+  applyMiddleware(logger),
+);

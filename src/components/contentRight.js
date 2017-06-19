1ffcc9560a39edf1e5e04d10b2b78e2f4b82d67d
Adriene Cranny <adrienecranny@HDC-WRK-M62.local>
2017-06-19 14:32:04 -0700

    Toggle signup button added.

--- src/components/contentRight.js
+++ src/components/contentRight.js
@@ -8,8 +8,9 @@ const ContentRight = () => (
   <div className="RightContent">
     <div className="mobile-login">
       <div className="login-title">
+        <p className="titleSubtext">Existing user?</p>
         <input id="toggle-input" type="checkbox" value="selected" />
-        <label className="toggle-label" htmlFor="toggle-input">Existing user? Log in below</label>
+        <label className="toggle-label" htmlFor="toggle-input">Click to login</label>
         <div className="toggle-login">
           <Login classStyle="mobile-loginElements" />
         </div>
@@ -18,8 +19,14 @@ const ContentRight = () => (
     <div className="RightHeader">
       <h1 className="sign-up">Sign Up</h1>
       <p className="titleSubtext">It&apos;s free and always will be.</p>
+      <input type="checkbox" value="selected" id="toggle-input2" />
+      <label className="signup-label" htmlFor="toggle-input2">
+      Click to Sign up!
+      </label>
+      <div className="toggle-signup">
+        <Signup />
+      </div>
     </div>
-    <Signup />
   </div>
 );
 
